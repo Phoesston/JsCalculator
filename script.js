48060b76309736491633676fc62ab9a70f97fc3c
Joshua <joshua.jr2017@gmail.com>
2024-05-09 18:22:35 -0500

    Added a ui, still have some bugs to fix

--- script.js
+++ script.js
@@ -40,48 +40,79 @@ let computerScore = 0;
 
 function playRound(getHumanChoice,getComputerChoice){
 
-    //console.log("Human: "+getHumanChoice);
-    //console.log("Computer: "+getComputerChoice);
     if(getHumanChoice == "ROCK" && getComputerChoice == "SCISSORS"||
     getHumanChoice=="PAPER" && getComputerChoice=="ROCK"||
     getHumanChoice=="SCISSORS" && getComputerChoice=="PAPER"){
         console.log("You chose: " + getHumanChoice);
         console.log("Computer Chose: " + getComputerChoice);
         console.log("You Win!!\n\n");
+        document.getElementById("computerChoice").innerHTML = "<img src=\"./images/rock.png\" alt=\"Rock\">";
+        
         humanScore++;
+        document.getElementById("hScore").innerHTML = humanScore;
+        document.getElementById("cScore").innerHTML = computerScore;
     }else if(getHumanChoice==getComputerChoice){
         console.log("You chose: " + getHumanChoice);
         console.log("Computer Chose: " + getComputerChoice);
         console.log("It's a tie\n\n");
+        document.getElementById("computerChoice").innerHTML = "<img src=\"./images/paper.png\" alt=\"Paper\">";
+        document.getElementById("hScore").innerHTML = humanScore;
+        document.getElementById("cScore").innerHTML = humanScore;
     }else{
         console.log("You chose: " + getHumanChoice);
         console.log("Computer Chose: " + getComputerChoice);
         console.log("You Lose\n\n");
+        document.getElementById("computerChoice").innerHTML = "<img src=\"./images/scissors.png\" alt=\"Scissors\">";
         computerScore++;
+
+        document.getElementById("hScore").innerHTML = humanScore;
+        document.getElementById("cScore").innerHTML = computerScore;
     }
 
 }
 
-/*function playGame(){
-    let Rounds = 5;
-    let currentRound =1;
-    
-    for(currentRound;currentRound<(Rounds+1); currentRound++){
-        console.log("Round: "+currentRound);
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
+let Rounds = 5;
+let currentRound =1;
 
+function playGame(humanChoice,computerChoice){
+    
+    document.getElementById("currRound").innerHTML = currentRound;
+    if(currentRound<=Rounds){
         playRound(humanChoice,computerChoice);
+        currentRound++;
 
     }
 
-  
-}*/
+}
 
-playGame();
+document.addEventListener("DOMContentLoaded", function(){
 
-console.log("human score: "+humanScore);
-console.log("computer score: "+computerScore);
+    const rockBtn = document.getElementById("rockBtn");
+    const paperBtn = document.getElementById("paperBtn");
+    const scissorsBtn = document.getElementById("scissorsBtn");
+
+    
+
+    rockBtn.addEventListener("click",function(){
+        const computerChoice = getComputerChoice();
+        playGame("ROCK",computerChoice);
+    });
+
+    paperBtn.addEventListener("click",function(){
+        const computerChoice = getComputerChoice();
+        playGame("PAPER",computerChoice);
+    });
+
+    scissorsBtn.addEventListener("click",function(){
+        const computerChoice = getComputerChoice();
+        playGame("SCISSORS",computerChoice);
+    });
+});
+
+
+
+//console.log("human score: "+humanScore);
+//console.log("computer score: "+computerScore);
 
 
 
